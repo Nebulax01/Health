8450ad1b19cff53d6eaf5fb14232ccfcac018567
Nebulax01 <akrammalekess@gmail.com>
2023-04-09 01:11:18 +0100

    added functionalities : basic info page , diseases, vaccinations, allergies and database models

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,13 +1,33 @@
 import { Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
  import { AuthGuard } from '@nestjs/passport'; import { Request } from 'express'; 
 import { ProfileService } from './profile.service';
+import { Allergy, Disease, PatientProfile } from '@prisma/client';
+
+
+
  @Controller('profile') 
  export class ProfileController {
+
      constructor(private ProfileS: ProfileService){ }
+
+
   @Post('basic-info') 
   @HttpCode(HttpStatus.OK)
-   @UseGuards(AuthGuard('jwt')) 
-   async Basic_info(@Req() req: Request){ 
-    return await this.ProfileS.BasicP(req.user['sub']); 
-} 
+  @UseGuards(AuthGuard('jwt')) 
+   async Basic_info(@Req() req: Request): Promise<PatientProfile>{ 
+        return await this.ProfileS.BasicP(req.user['sub']); 
+    } 
+
+    @Post('diseases')
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(AuthGuard('jwt')) 
+    async diseases(@Req() req: Request) : Promise<Disease[]>{
+        return await this.ProfileS.diseases(req.user['sub']);
+    }
+    @Post('allergies')
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(AuthGuard('jwt')) 
+    async allergies(@Req() req: Request) : Promise<Allergy[]>{
+        return await this.ProfileS.allergies(req.user['sub']);
+    }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -2,20 +2,58 @@ import { Injectable, Req } from '@nestjs/common';
 import { Request } from 'express';
  import { PrismaService } from 'src/prisma/prisma.service'; 
  import { BasicInfo } from 'src/basic-u/basic-u.interface';
+import { Allergy, Disease, PatientProfile, User, Vaccination } from '@prisma/client';
 @Injectable() 
 export class ProfileService { 
     constructor(private prisma: PrismaService){ } 
-    async BasicP(id: number){
+    async BasicP(id: number): Promise<PatientProfile>{
          const usr = await this.prisma.patientProfile.findUnique({
              where:{
                 user_id: id
               } 
             }) ;
+            
+        
 
-            // const Buser : BasicInfo = {
-            //     name : usr.full_name
-    
-            // }
+           return usr;
         } 
+
+
+        async diseases (id: number): Promise<Disease[]>{
+            const patient = await this.prisma.patientProfile.findUnique({
+                where:{
+                    user_id : id
+                },
+                include:{
+                    diseases: true,
+                }
+            });
+
+            return patient.diseases;
+        }
+        async allergies (id: number): Promise<Allergy[]>{
+            const patient = await this.prisma.patientProfile.findUnique({
+                where:{
+                    user_id : id
+                },
+                include:{
+                    allergies: true,
+                }
+            });
+
+            return patient.allergies;
+        }
+        async vaccinations (id: number): Promise<Vaccination[]>{
+            const patient = await this.prisma.patientProfile.findUnique({
+                where:{
+                    user_id : id
+                },
+                include:{
+                    vaccinations: true,
+                }
+            });
+
+            return patient.vaccinations;
+        }
         
     }
